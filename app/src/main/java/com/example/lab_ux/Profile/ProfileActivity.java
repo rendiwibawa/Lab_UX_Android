4a529dc125948de4b2e3848bff0910a80260476d
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2024-08-07 18:50:09 +0700

    Update tampilan login dan sedikit animasi popup

--- app/src/main/java/com/example/lab_ux/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/lab_ux/Profile/ProfileActivity.java
@@ -2,10 +2,16 @@ package com.example.lab_ux.Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.lab_ux.Login.LoginActivity;
 import com.example.lab_ux.R;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -25,5 +31,45 @@ public class ProfileActivity extends AppCompatActivity {
         usernameTextView.setText(username);
         emailTextView.setText(email + "@gmail.com");
 
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLogoutDialog();
+            }
+        });
+
+    }
+
+    private void showLogoutDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_logout_dialog);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView cancelButton = dialog.findViewById(R.id.cancelButton);
+        TextView okButton = dialog.findViewById(R.id.okButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(ProfileActivity.this, R.anim.flip_up_out));
+                dialog.dismiss();
+            }
+        });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(ProfileActivity.this, R.anim.flip_up_out));
+                dialog.dismiss();
+                // Handle logout action here, e.g., navigate to login screen
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        dialog.show();
+        dialog.findViewById(R.id.dialog_container).startAnimation(AnimationUtils.loadAnimation(this, R.anim.flip_up_in));
     }
 }
